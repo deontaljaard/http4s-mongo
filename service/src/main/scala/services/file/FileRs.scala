fd11151c45ba6419c98708b8bbfb4b1cc3d79f00
Deon Taljaard <deon.subscriptions@gmail.com>
2017-11-05 21:46:44 +0200

    Add rough code to decode first part of multipart containing file metadata as json payload.

--- service/src/main/scala/services/file/FileRs.scala
+++ service/src/main/scala/services/file/FileRs.scala
@@ -1,19 +1,80 @@
 package services.file
 
+import java.nio.charset.CharacterCodingException
+
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.headers._
+import fs2.Task
+import org.http4s.MediaType._
 import org.http4s._
 import org.http4s.dsl._
-import org.http4s.multipart.Multipart
+import org.http4s.circe.jsonEncoderOf
+import org.http4s.multipart.{Boundary, Multipart, Part}
+import scodec.bits.ByteVector
+import io.circe._, io.circe.parser._
 
 object FileRs {
   val FILES: String = "files"
 
+  case class JsonMetaData(userId: String)
+
+  case class MultipartResponse(userId: String, fileName: String, contentType: `Content-Type`)
+
   val service = HttpService {
-    case req @ POST -> Root / FILES / "upload" =>
+    case req@POST -> Root / FILES / "upload" =>
       val v = for {
         t <- req.as[Multipart]
-        _ = processMultipartRequest(req)
+        _ = processMultipart(t)
       } yield Ok()
       v.flatMap(identity)
+  }
+
+  private def processMultipart(multipart: Multipart): Unit = {
+
+    implicit def jsonMetaDataEncoder: EntityEncoder[JsonMetaData] = jsonEncoderOf[JsonMetaData]
+
+    val headers: Headers = multipart.headers
+    println(s"Headers: $headers")
+
+    val boundary: Boundary = multipart.boundary
+    println(s"boundary: $boundary")
+
+    val parts: Vector[Part] = multipart.parts
+    println(s"Parts: $parts")
+
+    val vector = parts.map { part =>
+      part.headers.get(`Content-Type`) match {
+        case Some(`Content-Type`(`application/json`, _)) =>
+          println("Process JSON payload")
+          val jsonAsString = part.body.runLog.map(ByteVector(_))
+            .map(_.decodeUtf8)
+            .map(_.fold(g
+              { ex => println(ex); None },
+              { s =>
+                parse(s) match {
+                  case Left(e) => println(e); None
+                  case Right(_) => decode[JsonMetaData](s) match {
+                    case Left(error) => println(error); None
+                    case Right(js: JsonMetaData) => Some(js)
+                  }
+                }
+              })
+            )
+          println(s"---- ${jsonAsString.unsafeRun}")
+        case Some(`Content-Type`(`image/png`, _)) =>
+          println("Process image")
+          val imageByteVector: Task[ByteVector] = part.body.runLog.map(ByteVector(_))
+          println(s"Image byte vector ${imageByteVector.unsafeRun}")
+          val imageByteArray = imageByteVector.map(_.toArray)
+          println(s"Image byte array ${imageByteArray.unsafeRun.length}")
+        case _ => println("Unsupported content type in part. Ignoring.")
+      }
+      val value: Task[Either[CharacterCodingException, String]] = part.body.runLog.map(ByteVector(_)).map(_.decodeUtf8)
+      value.unsafeValue
+    }
+
+    println("Decoded multipart" + vector)
   }
 
   private def processMultipartRequest(request: Request): Unit = {
--- service/src/test/scala/services/file/FileRsTest.scala
+++ service/src/test/scala/services/file/FileRsTest.scala
@@ -1,10 +1,17 @@
 package services.file
 
-import fs2._
+import java.io.File
+
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.MediaType._
 import org.http4s._
+import org.http4s.circe.jsonEncoderOf
 import org.http4s.headers._
+import org.http4s.multipart.{Multipart, Part}
 import org.specs2.Specification
 import org.specs2.matcher.{ThrownExpectations, ThrownMessages}
+import services.file.FileRs.JsonMetaData
 
 class FileRsTest extends Specification
   with ThrownExpectations
@@ -15,7 +22,7 @@ class FileRsTest extends Specification
    This is a specification to check the 'File RESTful Service'
 
    The 'File RESTful Service' should
-     return a greeting containing the path param               $uploadFile"""
+//  return a greeting containing the path param               $uploadFile"""
 
   val fileRs: HttpService = FileRs.service
 
@@ -34,15 +41,32 @@ class FileRsTest extends Specification
       ------WebKitFormBoundarycaZFo8IAKVROTEeD--
             """.replaceAllLiterally("\n", "\r\n")
     val header = Headers(`Content-Type`(MediaType.multipart("form-data", Some("----WebKitFormBoundarycaZFo8IAKVROTEeD"))))
+//
+//    val value1: Stream[Task, Byte] = Stream.emit(body).through(text.utf8Encode)
 
-    val value1: Stream[Task, Byte] = Stream.emit(body).through(text.utf8Encode)
+//    val uploadRequest = Request(
+//      method = Method.POST,
+//      uri = Uri.uri("files/upload"),
+//      body = value1,
+//      headers = header)
 
+    implicit def jsonMetaDataEncoder: EntityEncoder[JsonMetaData] = jsonEncoderOf[JsonMetaData]
+
+    val path = getClass.getResource("/http4s.png").getPath
+    val file = new File(path)
+    val jsonMetaData = JsonMetaData("76293429")
+    val json = jsonMetaData.asJson.toString
+    val part1 = Part.formData("test-metadata", json, `Content-Type`(`application/json`))
+    val part2 = Part.fileData("test-file", file, `Content-Type`(`image/png`))
+    val multipart = Multipart(Vector(part1, part2))
+    val multipartEntity = EntityEncoder[Multipart].toEntity(multipart)
+    val multipartBody = multipartEntity.unsafeRun.body
 
     val uploadRequest = Request(
       method = Method.POST,
       uri = Uri.uri("files/upload"),
-      body = value1,
-      headers = header)
+      body = multipartBody,
+      headers = multipart.headers)
 
     val response = fileRs(uploadRequest).unsafeRun
 
